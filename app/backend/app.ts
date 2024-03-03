d23031c979b8303bef9272366154866e128777f3
ashsic <99445200+ashsic@users.noreply.github.com>
2024-03-03 11:10:42 -0500

    app compiles and sends data on base route

--- app/backend/app.ts
+++ app/backend/app.ts
@@ -1,9 +1,19 @@
 import express from 'express';
-import { connectDb } from './models/index.js';
+import { connectDb } from './models/index';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+
+// Routes
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+
+
 
 connectDb().then(() => {
   console.log('Connected to MongoDB.');
