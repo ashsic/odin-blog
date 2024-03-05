90b30e36e281af380b0a4ab980b5c030802915f5
ashsic <99445200+ashsic@users.noreply.github.com>
2024-03-05 12:57:27 -0500

    feat: can now get/post to /posts route

--- app/backend/models/Post.ts
+++ app/backend/models/Post.ts
@@ -4,7 +4,7 @@ const postSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     title: { type: String, required: true },
-    body: { type: String, required: true },
+    bodytext: { type: String, required: true },
   },
   { timestamps: true }
 );
--- app/backend/routes/Post.ts
+++ app/backend/routes/Post.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
+import CustomRequest from '../interfaces/CustomRequest';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('test');
-  // res.send(Object.values(req.context.models.users));
+router.get('/', async (req: CustomRequest, res) => {
+  const posts = await req.context?.models?.Post?.find({}) ?? 'Undefined';
+  return res.send(posts);
+});
+
+router.post('/', async (req: CustomRequest, res) => {
+  const Post = await req.context?.models?.Post;
+  const post = new Post({
+    user: req.body.user,
+    title: req.body.title,
+    bodytext: req.body.bodytext,
+  });
+
+  await post.save()
+
+  return res.send(post);
 });
 
 export default router;
