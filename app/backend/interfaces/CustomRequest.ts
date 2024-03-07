61f869495d11df16b340d0a5ef3e388bbdb3272e
ashsic <99445200+ashsic@users.noreply.github.com>
2024-03-07 09:41:07 -0500

    removing CustomRequest interface as it is not needed

--- app/backend/interfaces/CustomRequest.ts
+++
@@ -1,8 +0,0 @@
-import { Request } from 'express';
-import { Model, Document } from 'mongoose';
-
-export default interface CustomRequest extends Request {
-  context?: {
-    models?: any
-  };
-};
